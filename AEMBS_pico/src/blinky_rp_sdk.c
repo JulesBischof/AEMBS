aa6a2003b45c70509c3d872c096646257d6a8a09
Julian Bischof <Julian-Bischof@hotmail.com>
2025-02-18 22:15:19 +0100

    Aufgabe Woche 1 erledigt

--- AEMBS_pico/src/blinky_rp_sdk.c
+++ AEMBS_pico/src/blinky_rp_sdk.c
@@ -10,19 +10,56 @@
 #include "platform.h"
 
 static void delay(void) {
-  for(volatile int i=0; i<5000000; i++) {
+  for(volatile int i=0; i<3000000; i++) {
       __asm("nop");
   }
 }
+
+
+void threeShort(void) {
+    for(int i = 0; i < 3; i++) {
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        delay();
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        delay();
+    }
+}
+
+void threelong(void) {
+    for(int i = 0; i < 3; i++) {
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        delay();
+        delay();
+        delay();
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        delay();
+    }
+}
+
 
 void blinky_sdk_rp(void) {
     const unsigned int LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while(1)
+    {
+        threeShort();
+        threelong();
+        threeShort();
+
+        delay();
+        delay();
+        delay();
+    }
+
+
+    /*
     for(;;) {
         gpio_put(LED_PIN, 0);
         delay();
         gpio_put(LED_PIN, 1);
         delay();
     }
+    */
 }
